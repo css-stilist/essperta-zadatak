7c5ff963855880cf78f883a79b4e2cb69ea3ab3e
Mladen Panić <mladen.panic@profil-klett.hr>
2021-07-26 23:29:19 +0200

    Poll results chart

--- src/components/PollResults.vue
+++ src/components/PollResults.vue
@@ -1,18 +1,103 @@
 <template>
-  <div class="poll-results">
-    <h1>Rezultat</h1>
+  <div class="poll-results" :class="elClass">
+
+    <poll-results-chart v-if="!isLoading" :chartdata="results"></poll-results-chart>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Opcije</th><th>Odgovori</th><th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in rawData" :key="row.label">
+          <td>{{row.label}}</td>
+          <td>{{row.value}}</td>
+          <td>{{row.count}}</td>
+        </tr>
+      </tbody>
+    </table>
+
   </div>
 </template>
 
 <script>
+import PollResultsChart from './PollResultsChart.vue';
+
 export default {
   name: 'poll-results',
+  components: {
+    'poll-results-chart': PollResultsChart,
+  },
   data() {
-    return {};
+    return {
+      isLoading: true,
+      rawData: [
+        { label: 'Po preporuci', value: 30, count: 9 },
+        { label: 'Svidio mi se sadržaj', value: 22, count: 9 },
+        { label: 'Želim produbiti znanje o tematici', value: 24, count: 9 },
+        { label: 'Po zahtjevu poslodavca', value: 38, count: 9 },
+        { label: 'Mislim da će mi pomoći u razvoju karijere', value: 16, count: 9 },
+        { label: 'Drugi razlozi', value: 8, count: 9 },
+      ],
+      results: {
+        labels: [
+        ],
+        datasets: [
+          {
+            label: 'Rezultati',
+            data: [],
+          },
+        ],
+      },
+    };
+  },
+  computed: {
+    elClass() {
+      return this.isLoading ? 'is-loading' : '';
+    },
+  },
+  mounted() {
+    // fake getting data from API (so we can see loading state)
+    // and format data for use with chart plugin
+    setTimeout(() => {
+      this.rawData.forEach((row) => {
+        this.results.labels.push(row.label);
+        this.results.datasets[0].data.push(row.value);
+      });
+      this.isLoading = false;
+    }, 1500);
   },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.poll-results {
+  min-height: 400px;
+  position: relative;
 
+  * {
+    opacity: 1;
+    transition: all 0.3s;
+  }
+}
+
+.is-loading {
+  * {
+    opacity: 0;
+  }
+
+  &:before {
+    content: "Loading data...";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 26px;
+  }
+}
 </style>
---
+++ src/components/PollResultsChart.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="poll-results-chart"></div>
+</template>
+<script>
+import ApexCharts from 'apexcharts';
+
+export default {
+  name: 'poll-results-chart',
+  props: ['chartdata'],
+  mounted() {
+    const options = {
+      series: [{
+        data: this.chartdata.datasets[0].data,
+      }],
+      // colors: ['#ccc'],
+      chart: {
+        type: 'bar',
+        height: 350,
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 4,
+          horizontal: true,
+          colors: {
+            // backgroundBarColors: ['#ff0000', '#0000ff', '#cccccc', '#00ff00'],
+          },
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      xaxis: {
+        categories: this.chartdata.labels,
+      },
+    };
+
+    const chart = new ApexCharts(document.querySelector('#poll-results-chart'), options);
+    chart.render();
+  },
+};
+</script>
+
+<style>
+</style>
